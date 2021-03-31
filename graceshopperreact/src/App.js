def2919c2bf74d89881e560f8a84437cbbe0123d
Corey Frederickson <coreyfrederickson@Coreys-MBP.attlocal.net>
2021-03-31 14:16:53 -0500

    Started navBar

--- graceshopperreact/src/App.js
+++ graceshopperreact/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter as Router, Link, Switch, Route} from "react-router-dom"
+import {getToken, clearToken} from "./auth"
 
-function App() {
+const App = () => {
+  const [authorized, setAuthorized] = useState(false);
+  const [currentUser, setCurrentUser] = useState("");
+  const [loggedIn, setLoggedIn] = useState(getToken());
+  const [activities, setActivities] = useState(null);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav className="navBar">
+        <h1>Grace-Shopper</h1>
+        <div>
+          <Link className="Link" to= '/'>Home</Link>
+          <Link className="Link" to= '/products'>Shop</Link>
+          <Link className="Link" to= '/cart'>Cart</Link>
+          <Link className="Link" to= '/orders'>Orders</Link>
+          <Link className="Link" to= '/login'>Login</Link>
+          <Link className="Link" to= '/register'>Sign Up</Link>
+          <Link className="Link" to= '/admin'>Admin Link</Link>
+          <Link className="Link" to= '/profile'>My Profile</Link>
+
+          {/* {loggedIn ? <Link className="Link" onClick={() => {
+                        clearToken();
+                        //setUsername(null);
+                        setLoggedIn(null);
+                        setAuthorized(null)
+                        setCurrentUser(null)
+                    }}
+                        to='/'>Log Out</Link> : null} */}
+        </div>
+      </nav>
+      <main>
+        <Switch>
+          <Route exact path= '/'>
+          </Route>
+          
+          <Route path='/Login'>
+          </Route>
+          
+          <Route path='/Register'>
+          </Route>
+          
+          <Route path='/routines'>
+          </Route>
+
+          <Route path='/myRoutines'>
+          </Route>
+          
+          <Route path='/activities'>
+          </Route>
+          
+          <Route path='/createRoutine'>
+          </Route> 
+
+        </Switch>  
+      </main>
+    </Router>
   );
 }
 
 export default App;
+
