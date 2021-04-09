df9be842e5dd13a5bffe019351a63c9810188c8a
Corey Frederickson <coreyfrederickson@Coreys-MacBook-Pro.local>
2021-04-09 00:30:37 -0500

    AdminCreateProduct barebones

--- graceshopperreact/src/components/AdminCreateProduct.js
+++ graceshopperreact/src/components/AdminCreateProduct.js
@@ -3,13 +3,58 @@ import { Redirect } from 'react-router';
 import {adminCreateProduct} from '../api'
 
 const AdminCreateProduct = ({isAdmin}) =>{
+    const [product, setProduct] = useState({imageLink: null})
 
+
+
+    const handleSubmit = async (evt)=>{
+        evt.preventDefault();
+        try {
+            const response = await adminCreateProduct(product)
+            console.log(response)
+            
+        }catch (error) {
+            throw error
+        }
+
+
+    }
     if (!isAdmin ){
         return <Redirect to="/" />
     }else{
         return(
             <div>
-            <h1>Add Product</h1>
+                <h1>Add Product</h1>
+                <form onSubmit={handleSubmit}>
+                    <label>Album:</label>
+                    <input
+                    //name="Title"
+                    required
+                    onChange={(e) => setProduct({ ...product, title: e.target.value })}
+                    />
+                    <label>Image Link:</label>
+                    <input onChange={(e) => setProduct({ ...product, imageLink: e.target.value })} />
+                    
+                    <label>Artist:</label>
+                    <input required onChange={(e) => setProduct({ ...product, artist: e.target.value })} />
+
+                    <label>Genre:</label>
+                    <input required onChange={(e) => setProduct({ ...product, genre: e.target.value })} />
+
+                    <label>Release Date:</label>
+                    <input required onChange={(e) => setProduct({ ...product, releaseDate: e.target.value })} />
+
+                    <label>Description:</label>
+                    <input required onChange={(e) => setProduct({ ...product, description: e.target.value })} />
+
+                    <label>Price:</label>
+                    <input required onChange={(e) => setProduct({ ...product, price: e.target.value })} />
+
+                    <label>Quantity:</label>
+                    <input required onChange={(e) => setProduct({ ...product, quantity: e.target.value })} />
+
+                    <button type="submit">submit</button>
+            </form>
             </div>
         )
     }
