ea4f5adff2716194e4868861c2e0afb0ca2d01e2
okristenicole9 <okristenicole9@yahoo.com>
2021-04-19 18:24:55 -0500

    seed and footer

--- graceshopperreact/src/index.js
+++ graceshopperreact/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Footer from './Footer';
 
 
 ReactDOM.render(
-    <App />,
+    [<App />, <Footer />],
   document.getElementById('root')
 );
 
