a7d3d321f9427f676863cae8a9210e1f931a6c0b
okristenicole9 <okristenicole9@yahoo.com>
2021-04-19 21:15:04 -0500

    today

--- graceshopperreact/src/Footer.js
+++ graceshopperreact/src/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
   <footer className="footer">
-    <p>Founders: Kristen, Corey, and David</p>
+    <h1>Founders: Kristen, Corey, and David</h1>
   </footer>
 );
 
--- graceshopperreact/src/components/IntroPage.js
+++ graceshopperreact/src/components/IntroPage.js
@@ -4,9 +4,17 @@ import { React } from "react";
 const IntroPage = () =>{
 
     return (<div>
-              <div className="home">
-                  Welcome to the best record store in the south! Our three founders Kristen, Corey, and David wanted to create a digital space for those who enjoy listening to their favorite records. These days it is hard to find records in store, therefore we have countless records avalable at your fingertips.
-              </div>
+              <div className="registerr" >
+                  <h1 className="ftr">For The Record </h1>
+                  <div className="intropage">
+                  <p>Welcome to the best rated record store in the south! Our three founders Kristen, Corey, and David wanted to create a digital space for those who enjoy listening to their favorite records. We know that these days it is hard to find records in store, therefore we have countless records avalable at your fingertips.</p>
+                  </div>
+                  <div className="img">
+                  <img alt="imageLink" src="https://upload.wikimedia.org/wikipedia/en/c/c4/Supertramp_-_Breakfast_in_America.jpg"/>
+                  <img alt="imageLink" src="https://upload.wikimedia.org/wikipedia/en/0/02/Queen_Greatest_Hits.png"/>
+                  <img alt="imageLink" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/ACDC_Back_in_Black.png/220px-ACDC_Back_in_Black.png"/>
+                  <img alt="imageLink" src="https://upload.wikimedia.org/wikipedia/en/0/00/Eagles_-_Their_Greatest_Hits_%281971-1975%29.jpg"/></div>
+                  </div>
             </div>)
 }
 
